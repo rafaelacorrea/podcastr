485fa5e7001616557fa8e6dd92c62df5d146713d
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2021-11-02 18:05:22 -0300

    feat: adding type HomeProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import { GetStaticsProps } from 'next';
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props:HomeProps) {
   return (
     <div>
       <h1>Index</h1>
@@ -8,8 +20,8 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
+export const getStaticProps: GetStaticsProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc')
   const data = await response.json()
 
   return{
